0ae4a1aa024cbcbbf9f58a8d1e5fe4332139d3ca
Scott Talbot <s@chikachow.org>
2016-03-26 11:49:04 +1100

    hack

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Git2-Autoinitialize",
+    name: "Git2Autoinitialize",
     dependencies: [
       .Package(url: "https://github.com/cysp/swift-CGit2.git", Version(0, 0, 0)),
     ]
