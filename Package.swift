410ad9ddefcd2ac4dd5a196e1db0a6151c05a734
Scott Talbot <s@chikachow.org>
2016-03-27 14:23:08 +1100

    staticlibraryhack

--- Package.swift
+++ Package.swift
@@ -6,3 +6,6 @@ let package = Package(
       .Package(url: "https://github.com/cysp/swift-CGit2.git", Version(0, 1, 0)),
     ]
 )
+
+let archive = Product(name: "Git2Autoinitialize", type: .Library(.Static), modules: "Git2Autoinitialize")
+products.append(archive)
